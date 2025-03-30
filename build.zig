6cf42364bcd788102ec2d329fd53f85ba9631a45
Jorri Fransen <jorrifransen@gmail.com>
2025-03-30 21:59:01 +0200

    Installing shaders next to main exe.

--- build.zig
+++ build.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
+const dlog = std.log.debug;
 
-pub fn build(b: *std.Build) void {
+const builtin = @import("builtin");
+
+pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "v10game",
         .root_source_file = b.path("src/main.zig"),
@@ -8,6 +11,11 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
+    run_step.dependOn(b.getInstallStep());
 
     const glfw_dep = b.dependency("glfw", .{ .target = b.graph.host, .x11 = true, .wayland = true });
     const glfw_lib = glfw_dep.artifact("glfw");
@@ -21,26 +29,45 @@ pub fn build(b: *std.Build) void {
     }).module("vulkan-zig");
     exe.root_module.addImport("vulkan", vulkan_mod);
 
+    const shader_step = try addShaderStep(b);
+    exe.step.dependOn(shader_step);
+
+    const clean_step = b.step("clean", "Clean shaders and zig-out directory");
+    clean_step.dependOn(&b.addRemoveDirTree(std.Build.LazyPath{ .cwd_relative = b.install_path }).step);
+    if (builtin.os.tag != .windows) {
+        clean_step.dependOn(&b.addRemoveDirTree(std.Build.LazyPath{ .cwd_relative = b.cache_root.path.? }).step);
+    }
+}
+
+fn addShaderStep(b: *std.Build) !*std.Build.Step {
     const shaders = [_][]const u8{
         "shaders/simple.vert",
         "shaders/simple.frag",
     };
 
-    const shaders_step = b.step("shaders", "Compile shaders");
-    exe.step.dependOn(shaders_step);
+    const shaders_step = b.step("shaders", "compile shaders");
+    const wf = b.addWriteFiles();
+    wf.step.name = "WriteFile shaders";
+    shaders_step.dependOn(&wf.step);
+
     for (shaders) |path| {
+        const spv_name = b.fmt("{s}.spv", .{path});
+
         const compile_step = b.addSystemCommand(&.{"glslc"});
         compile_step.addFileArg(b.path(path));
-        compile_step.addArgs(&.{ "-o", b.fmt("{s}.spv", .{path}) });
-        shaders_step.dependOn(&compile_step.step);
+        const spv_cache_path = compile_step.addPrefixedOutputFileArg("-o", spv_name);
+
+        _ = wf.addCopyFile(spv_cache_path, spv_name);
     }
 
-    // const glslc_step = b.addSystemCommand(&.{"glslc"});
-    // glslc_step.addFileArg(b.path("shaders/simple.vert"));
-    // glslc_step.addArgs(&.{ "-o", "shaders/simple.vert.spv" });
-    // exe.step.dependOn(&glslc_step.step);
+    const shader_install_dir = b.addInstallDirectory(.{
+        .source_dir = wf.getDirectory(),
+        .install_dir = .bin,
+        .install_subdir = "",
+    });
 
-    const run_exe = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_exe.step);
+    // b.getInstallStep().dependOn(&shader_install_dir.step);
+    shaders_step.dependOn(&shader_install_dir.step);
+
+    return shaders_step;
 }
