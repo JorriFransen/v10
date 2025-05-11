ca84030048f686c9ad2bf364aa25a3521733e0e1
Jorri Fransen <jorrifransen@gmail.com>
2025-05-12 01:50:08 +0200

    Virtual arena init.

--- src/memory/arena.zig
+++ src/memory/arena.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const mem = @import("../memory.zig");
 
 const Allocator = std.mem.Allocator;
 const Alignment = std.mem.Alignment;
@@ -16,30 +17,80 @@ pub const Arena = struct {
 
     pub const Flags = packed struct(u8) {
         @"align": bool = true,
-        grow: bool = false,
+        /// reserved_virtual_address_space
+        rvas: bool = false,
         __reserved__: u6 = 0,
     };
 
-    pub const InitOptions = struct {
-        flags: Flags = .{},
+    pub const InitOptions = union(enum) {
+        pub const Virtual = struct {
+            flags: Flags = .{ .rvas = true },
+            reserved_capacity: usize,
+        };
+
+        slice: struct {
+            flags: Flags = .{ .rvas = false },
+            data: []u8,
+        },
+        virtual: Virtual,
     };
 
-    pub fn init(data: []u8, flags: Flags) Arena {
-        return .{
-            .data = data,
-            .used = 0,
-            .reserved_capacity = data.len,
-            .flags = flags,
-            .last_allocation = null,
-            .last_size = 0,
+    pub const InitError = error{
+        OutOfMemory,
+        AccessDenied,
+        Unexpected,
+    };
+
+    pub fn init(options: InitOptions) InitError!Arena {
+        return switch (options) {
+            .slice => |s| return .{
+                .data = s.data,
+                .used = 0,
+                .reserved_capacity = s.data.len,
+                .flags = s.flags,
+                .last_allocation = null,
+                .last_size = 0,
+            },
+            .virtual => |v| return init_virtual(v),
         };
     }
 
-    // TODO: Merge with init, add InitOptions...
-    pub fn create(flags: Flags) Arena {
-        std.debug.assert(false);
-        const data = [_]u8{};
-        return init(&data, flags);
+    fn init_virtual(options: InitOptions.Virtual) InitError!Arena {
+        std.debug.assert(options.flags.rvas);
+
+        const page_size = std.heap.pageSize();
+        std.debug.assert(options.reserved_capacity >= page_size);
+
+        if (builtin.os.tag == .linux) {
+            const data = std.posix.mmap(
+                null,
+                options.reserved_capacity,
+                std.c.PROT.NONE,
+                .{ .TYPE = .PRIVATE, .ANONYMOUS = true },
+                -1,
+                0,
+            ) catch |err| switch (err) {
+                error.OutOfMemory => return error.OutOfMemory,
+                else => return error.Unexpected,
+            };
+
+            std.posix.mprotect(data[0..page_size], std.c.PROT.READ | std.c.PROT.WRITE) catch |err| switch (err) {
+                error.OutOfMemory => return error.OutOfMemory,
+                error.AccessDenied => return error.AccessDenied,
+                error.Unexpected => return error.Unexpected,
+            };
+
+            return .{
+                .data = data,
+                .used = 0,
+                .reserved_capacity = options.reserved_capacity,
+                .flags = options.flags,
+                .last_allocation = null,
+                .last_size = 0,
+            };
+        } else {
+            @compileError("missing implementation for platforn for 'Arena.init_virtual'");
+        }
     }
 
     pub fn allocator(this: *@This()) Allocator {
@@ -55,7 +106,7 @@ pub const Arena = struct {
     }
 
     fn grow(this: *@This(), min_size: usize) bool {
-        if (!this.flags.grow) return false;
+        if (!this.flags.rvas) return false;
 
         _ = min_size;
         std.debug.assert(false);
@@ -81,7 +132,6 @@ pub const Arena = struct {
         }
 
         const unaligned_addr: usize = @intFromPtr(available.ptr);
-
         this.used += n;
 
         return @ptrFromInt(blk: {
@@ -91,8 +141,6 @@ pub const Arena = struct {
                 break :blk r;
             } else break :blk unaligned_addr;
         });
-
-        // return @ptrFromInt(result);
     }
 
     pub fn resize(ctx: *anyopaque, memory: []u8, alignment: Alignment, new_len: usize, ret_addr: usize) bool {
@@ -126,17 +174,17 @@ pub const Arena = struct {
 };
 
 test "Arena from slice" {
-    var mem: [70]u8 align(8) = [_]u8{1} ** 70; // Needs to be bigger to account for alignment
-    try std.testing.expectEqual(@as(*u8, @ptrCast(&mem)), &mem[0]);
+    var buf: [70]u8 align(8) = [_]u8{1} ** 70; // Needs to be bigger to account for alignment
+    try std.testing.expectEqual(@as(*u8, @ptrCast(&buf)), &buf[0]);
 
-    var arena = Arena.init(&mem, .{});
+    var arena = try Arena.init(.{ .slice = .{ .data = &buf } });
     const aa = arena.allocator();
 
     const first = try aa.create(u8);
     const second = try aa.create(u8);
 
-    try std.testing.expectEqual(first, &mem[0]);
-    try std.testing.expectEqual(second, &mem[1]);
+    try std.testing.expectEqual(first, &buf[0]);
+    try std.testing.expectEqual(second, &buf[1]);
 
     if (@import("builtin").mode == .Debug) {
         try std.testing.expectEqual(0xaa, first.*);
@@ -146,12 +194,12 @@ test "Arena from slice" {
     first.* = 11;
     second.* = 22;
 
-    try std.testing.expectEqual(first.*, mem[0]);
-    try std.testing.expectEqual(second.*, mem[1]);
+    try std.testing.expectEqual(first.*, buf[0]);
+    try std.testing.expectEqual(second.*, buf[1]);
 
     const third = try aa.alignedAlloc(u8, Alignment.@"4", 2);
 
-    try std.testing.expectEqual(@as(*u8, @ptrCast(third.ptr)), &mem[4]);
+    try std.testing.expectEqual(@as(*u8, @ptrCast(third.ptr)), &buf[4]);
 
     if (@import("builtin").mode == .Debug) {
         try std.testing.expectEqualSlices(u8, &.{ 0xaa, 0xaa }, third);
@@ -163,48 +211,52 @@ test "Arena from slice" {
     third[1] = 44;
 
     try std.testing.expectEqualSlices(u8, &.{ 33, 44 }, third);
-    try std.testing.expectEqual(third[0], mem[4]);
-    try std.testing.expectEqual(third[1], mem[5]);
+    try std.testing.expectEqual(third[0], buf[4]);
+    try std.testing.expectEqual(third[1], buf[5]);
 }
 
 test "Arena uncommitted" {
-    // var arena = Arena.create(.{});
-    // const aa = arena.allocator();
-    //
-    // const first = try aa.create(u8);
-    // const second = try aa.create(u8);
-    //
-    // _ = first;
-    // _ = second;
+    const page_size = std.heap.pageSize();
+    var arena = try Arena.init(.{ .virtual = .{ .reserved_capacity = page_size } });
+    const aa = arena.allocator();
 
-    // try std.testing.expectEqual(first, &mem[0]);
-    // try std.testing.expectEqual(second, &mem[1]);
-    //
-    // if (@import("builtin").mode == .Debug) {
-    //     try std.testing.expectEqual(0xaa, first.*);
-    //     try std.testing.expectEqual(0xaa, second.*);
-    // }
-    //
-    // first.* = 11;
-    // second.* = 22;
-    //
-    // try std.testing.expectEqual(first.*, mem[0]);
-    // try std.testing.expectEqual(second.*, mem[1]);
-    //
-    // const third = try aa.alignedAlloc(u8, Alignment.@"4", 2);
-    //
-    // try std.testing.expectEqual(@as(*u8, @ptrCast(third.ptr)), &mem[4]);
-    //
-    // if (@import("builtin").mode == .Debug) {
-    //     try std.testing.expectEqualSlices(u8, &.{ 0xaa, 0xaa }, third);
-    //     try std.testing.expectEqual(0xaa, third[0]);
-    //     try std.testing.expectEqual(0xaa, third[1]);
-    // }
-    //
-    // third[0] = 33;
-    // third[1] = 44;
-    //
-    // try std.testing.expectEqualSlices(u8, &.{ 33, 44 }, third);
-    // try std.testing.expectEqual(third[0], mem[4]);
-    // try std.testing.expectEqual(third[1], mem[5]);
+    const first = try aa.create(u8);
+    const second = try aa.create(u8);
+
+    if (@import("builtin").mode == .Debug) {
+        try std.testing.expectEqual(0xaa, first.*);
+        try std.testing.expectEqual(0xaa, second.*);
+    }
+
+    first.* = 11;
+    second.* = 22;
+
+    try std.testing.expectEqual(11, first.*);
+    try std.testing.expectEqual(22, second.*);
+
+    const third = try aa.alignedAlloc(u8, Alignment.@"4", 2);
+
+    if (@import("builtin").mode == .Debug) {
+        try std.testing.expectEqualSlices(u8, &.{ 0xaa, 0xaa }, third);
+        try std.testing.expectEqual(0xaa, third[0]);
+        try std.testing.expectEqual(0xaa, third[1]);
+    }
+
+    third[0] = 33;
+    third[1] = 44;
+
+    try std.testing.expectEqualSlices(u8, &.{ 33, 44 }, third);
+    try std.testing.expectEqual(33, third[0]);
+    try std.testing.expectEqual(44, third[1]);
+
+    // fill remaining capacity
+    const remaining = arena.data.len - arena.used;
+    const remaining_data = try aa.alignedAlloc(u8, .@"1", remaining);
+
+    try std.testing.expectEqual(remaining, remaining_data.len);
+    try std.testing.expectEqual(arena.data.len, arena.used);
+
+    // should trigger grow/commit
+    const newmem = try aa.create(u64);
+    _ = newmem;
 }
