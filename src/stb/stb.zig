9e8ecaf9ef65c62f15fd565a85c2d27fe8f40ba9
Jorri Fransen <jorrifransen@gmail.com>
2025-07-13 23:57:27 +0200

    Loading textures into memory.

--- src/stb/stb.zig
+++ src/stb/stb.zig
@@ -5,8 +5,13 @@ pub const c = @cImport({
 });
 
 pub const stbi_load = f("stbi_load", fn (path: [*:0]const u8, x: *c_int, y: *c_int, c: *c_int, desired_c: c_int) callconv(.c) ?[*]const u8);
+pub const stbi_load_from_memory = f("stbi_load_from_memory", fn (buffer: [*]const u8, len: c_int, x: *c_int, y: *c_int, c: *c_int, desired_c: c_int) callconv(.c) ?[*]const u8);
 pub const stbi_image_free = f("stbi_image_free", fn (data: [*]const u8) callconv(.c) void);
 
 fn f(comptime name: []const u8, comptime T: type) *const T {
     return @extern(*const T, .{ .name = name, .library_name = "c" });
+}
+
+pub export fn zigAssert(condition: c_int) void {
+    std.debug.assert(condition != 0);
 }
