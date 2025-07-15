16e18181f35a601ab05330548caf5913deff8347
Jorri Fransen <jorrifransen@gmail.com>
2025-07-15 02:17:18 +0200

    Uploading textures to gpu.

--- src/gfx/device.zig
+++ src/gfx/device.zig
@@ -638,17 +638,73 @@ pub fn createBuffer(this: *@This(), size: vk.DeviceSize, usage: vk.BufferUsageFl
     return buffer;
 }
 
-pub fn copyBuffer(this: *@This(), src: vk.Buffer, dst: vk.Buffer, size: vk.DeviceSize) void {
-    const command_buffer = this.beginSingleTimeCommands();
-    defer this.endSingleTimeCommands(command_buffer);
-
+pub fn copyBuffer(_: *@This(), cb: *const vk.CommandBufferProxy, src: vk.Buffer, dst: vk.Buffer, size: vk.DeviceSize) void {
     const copy_region = vk.BufferCopy{
         .src_offset = 0,
         .dst_offset = 0,
         .size = size,
     };
 
-    command_buffer.copyBuffer(src, dst, 1, @ptrCast(&copy_region));
+    cb.copyBuffer(src, dst, 1, @ptrCast(&copy_region));
+}
+
+pub fn transitionImageLayout(_: *@This(), cb: *const vk.CommandBufferProxy, image: vk.Image, format: vk.Format, old_layout: vk.ImageLayout, new_layout: vk.ImageLayout) void {
+    const opts: struct {
+        src_access_mask: vk.AccessFlags,
+        dst_access_mask: vk.AccessFlags,
+        src_stage: vk.PipelineStageFlags,
+        dst_stage: vk.PipelineStageFlags,
+    } = if (old_layout == .undefined and new_layout == .transfer_dst_optimal) .{
+        .src_access_mask = .{},
+        .dst_access_mask = .{ .transfer_write_bit = true },
+        .src_stage = .{ .top_of_pipe_bit = true },
+        .dst_stage = .{ .transfer_bit = true },
+    } else if (old_layout == .transfer_dst_optimal and new_layout == .shader_read_only_optimal) .{
+        .src_access_mask = .{ .transfer_write_bit = true },
+        .dst_access_mask = .{ .shader_read_bit = true },
+        .src_stage = .{ .transfer_bit = true },
+        .dst_stage = .{ .fragment_shader_bit = true },
+    } else {
+        @panic("Invalid image transition");
+    };
+
+    const barrier = vk.ImageMemoryBarrier{
+        .old_layout = old_layout,
+        .new_layout = new_layout,
+        .src_queue_family_index = vk.QUEUE_FAMILY_IGNORED,
+        .dst_queue_family_index = vk.QUEUE_FAMILY_IGNORED,
+        .image = image,
+        .subresource_range = .{
+            .aspect_mask = .{ .color_bit = true },
+            .base_mip_level = 0,
+            .level_count = 1,
+            .base_array_layer = 0,
+            .layer_count = 1,
+        },
+        .src_access_mask = opts.src_access_mask,
+        .dst_access_mask = opts.dst_access_mask,
+    };
+
+    cb.pipelineBarrier(opts.src_stage, opts.dst_stage, .{}, 0, null, 0, null, 1, @ptrCast(&barrier));
+    _ = format;
+}
+
+pub fn copyBufferToImage(_: *@This(), cb: *const vk.CommandBufferProxy, buffer: vk.Buffer, image: vk.Image, width: u32, height: u32) void {
+    const region = vk.BufferImageCopy{
+        .buffer_offset = 0,
+        .buffer_row_length = 0,
+        .buffer_image_height = 0,
+        .image_subresource = .{
+            .aspect_mask = .{ .color_bit = true },
+            .mip_level = 0,
+            .base_array_layer = 0,
+            .layer_count = 1,
+        },
+        .image_offset = .{ .x = 0, .y = 0, .z = 0 },
+        .image_extent = .{ .width = width, .height = height, .depth = 1 },
+    };
+
+    cb.copyBufferToImage(buffer, image, .transfer_dst_optimal, 1, @ptrCast(&region));
 }
 
 pub fn beginSingleTimeCommands(this: *@This()) vk.CommandBufferProxy {
