1042a242cce438c41bf1a81c7f6b8b7df03d01fa
Jorri Fransen <jorrifransen@gmail.com>
2025-07-29 00:41:55 +0200

    Printing usage to File.

--- src/main.zig
+++ src/main.zig
@@ -36,11 +36,8 @@ pub fn main() !void {
     cli_options = clip.parse(CliOptionsType, mem.common_arena.allocator(), tmp.allocator()) catch return;
     tmp.release();
 
-    std.log.debug("cli_options: {}", .{cli_options});
-    std.log.debug("cli_options.test_str: '{s}'", .{cli_options.test_str});
-
     if (cli_options.help) {
-        clip.usage(CliOptionsType);
+        try clip.usage(CliOptionsType, std.fs.File.stdout());
         return;
     }
 
