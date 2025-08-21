8b3f5fc24fdb383772d208e4cdfcc818d1327298
Jorri Fransen <jorrifransen@gmail.com>
2025-08-21 21:51:44 +0200

    Enable test scene again.

--- src/main.zig
+++ src/main.zig
@@ -88,7 +88,6 @@ var kb_2d_move_controller: KB2DMoveController = .{};
 var entities: []Entity = &.{};
 var entity: *Entity = undefined;
 
-var test_font_bm: Font = undefined;
 var test_font_ttf: Font = undefined;
 
 var test_tile_texture: Texture = undefined;
@@ -147,8 +146,8 @@ fn run() !void {
     });
 
     // TrueType font
-    // test_font_ttf = try Font.load(&device, "res/fonts/ProFont/ProFont.ttf");
-    test_font_ttf = try Font.load(&device, "res/fonts/DejaVuSans/DejaVuSans.ttf", 72);
+    test_font_ttf = try Font.load(&device, "res/fonts/ProFont/ProFont.ttf", 72);
+    // test_font_ttf = try Font.load(&device, "res/fonts/DejaVuSans/DejaVuSans.ttf", 72);
     defer test_font_ttf.deinit(&device);
 
     test_tile_texture = try Texture.load(&device, "res/textures/test_tile.png", .{ .filter = .nearest });
@@ -242,33 +241,31 @@ fn drawFrame() !void {
 
         r2d.beginFrame(cb);
 
-        // const spos = window.getCursorPos();
+        const spos = window.getCursorPos();
         var batch = r2d.beginBatch(cb, &camera_2d);
         {
-            // drawDebugWorldGrid(&batch);
-            // drawTestScene(&batch);
+            drawDebugWorldGrid(&batch);
+            drawTestScene(&batch);
 
-            // const wpos = camera_2d.toWorldSpace(spos);
-            // batch.drawDebugLine(Vec2.scalar(0), wpos, .{});
+            const wpos = camera_2d.toWorldSpace(spos);
+            batch.drawDebugLine(Vec2.scalar(0), wpos, .{});
 
-            // batch.drawRect(Rect.new(Vec2.new(0, 0), Vec2.scalar(10)), .{});
+            const line_height: f32 = std.math.round(test_font_ttf.line_height + test_font_ttf.line_gap) / camera_2d.ppu;
 
-            // const line_height: f32 = std.math.round(test_font_ttf.line_height + test_font_ttf.line_gap) / camera_2d.ppu;
-            //
-            // batch.drawText(&test_font_ttf, Vec2.new(0, -line_height), "}<abc", .{});
-            // batch.drawText(&test_font_ttf, Vec2.new(0, 0), "}<abc", .{});
+            batch.drawText(&test_font_ttf, Vec2.new(0, -line_height), "}<abc", .{});
+            batch.drawText(&test_font_ttf, Vec2.new(0, 0), "}<abc", .{});
         }
         batch.end();
 
         var ui_batch = r2d.beginBatch(cb, &camera_ui);
         {
-            // const ui_pos = camera_ui.toWorldSpace(spos);
-            // ui_batch.drawDebugLine(Vec2.scalar(100), ui_pos, .{ .color = Vec4.new(1, 0, 0, 1) });
+            const ui_pos = camera_ui.toWorldSpace(spos);
+            ui_batch.drawDebugLine(Vec2.scalar(100), ui_pos, .{ .color = Vec4.new(1, 0, 0, 1) });
 
-            // const line_height: f32 = std.math.round(test_font_ttf.line_height + test_font_ttf.line_gap);
-            //
-            // ui_batch.drawText(&test_font_ttf, Vec2.new(10, 10), "}<abc", .{});
-            // ui_batch.drawText(&test_font_ttf, Vec2.new(10, 10 + line_height), "}<abc", .{});
+            const line_height: f32 = std.math.round(test_font_ttf.line_height + test_font_ttf.line_gap);
+
+            ui_batch.drawText(&test_font_ttf, Vec2.new(10, 10), "}<abc", .{});
+            ui_batch.drawText(&test_font_ttf, Vec2.new(10, 10 + line_height), "}<abc", .{});
         }
         ui_batch.end();
 
