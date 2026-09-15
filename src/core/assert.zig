92b597505dd4708844f69d95ad4caa31c2129a5d
Jorri Fransen <jorrifransen@gmail.com>
2026-09-15 22:57:04 +0200

    Gate assert on optimization mode.

--- src/core/assert.zig
+++ src/core/assert.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn assert(cond: bool) void {
     if (@inComptime()) {
         if (!cond) {
             @trap();
         }
-    } else {
+    } else if (builtin.mode == .Debug or builtin.mode == .ReleaseSafe) {
         if (!cond) {
             @branchHint(.cold);
             std.debug.dumpCurrentStackTrace(.{ .first_address = @returnAddress() });
